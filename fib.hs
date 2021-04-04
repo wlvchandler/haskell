f8606b8c05c14eaabe81f0b97a4ce71fd775e664
Will Chandler <3489539+wlvchandler@users.noreply.github.com>
2021-04-04 01:04:43 -0400

    Update fib.hs

--- fib.hs
+++ fib.hs
@@ -1,5 +1,8 @@
 import Data.List
 import Data.Bits
+import System.IO
+import System.CPUTime
+import Control.DeepSeq
 
 fib :: Int -> Integer
 fib n = snd . foldl_ fib_ (1, 0) . dropWhile not $
@@ -10,3 +13,9 @@ fib n = snd . foldl_ fib_ (1, 0) . dropWhile not $
             | otherwise = (ss, g*(2*f-g))
             where ss = f*f+g*g
         foldl_ = foldl' -- '
+
+main = do 
+  start <- getCPUTime
+  let r = fib 1000000000 
+  end <- deepseq r getCPUTime
+  putStrLn (show (end - start))
